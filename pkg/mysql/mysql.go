2f5da26c13dd7eb8ae4f765d4a61e987bec3364f
rafialfian10 <rafialfian770@gmail.com>
2023-03-05 08:11:35 +0700

    backend

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -14,7 +14,7 @@ func DatabaseInit() {
 	var err error
 
 	// username:root, password:kosong, route:localhost:3306, database name:projects
-	dsn := "root:SJICET9zDR0xrEjJic89@tcp(containers-us-west-134.railway.app:5847)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:xxumSGtJulD69dkIHhZ9@tcp(containers-us-west-134.railway.app:5847)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	// dsn := "root:@tcp(localhost:3306)/dewetour?charset=utf8mb4&parseTime=True&loc=Local"
 	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
