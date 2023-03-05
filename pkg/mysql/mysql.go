6e521ff65a14ef9515ba655f42c451fac2fe4384
rafialfian10 <rafialfian770@gmail.com>
2023-03-05 07:00:53 +0700

    backend

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -14,10 +14,10 @@ func DatabaseInit() {
 	var err error
 
 	// username:root, password:kosong, route:localhost:3306, database name:projects
-	// dsn := "root:44tlpl8sc0c4bQDC087y@tcp(containers-us-west-154.railway.app:6380)/railway?charset=utf8mb4&parseTime=True&loc=Local"
-	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	dsn := "root:@tcp(localhost:3306)/dewetour?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:SJICET9zDR0xrEjJic89@tcp(containers-us-west-134.railway.app:5847)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "root:@tcp(localhost:3306)/dewetour?charset=utf8mb4&parseTime=True&loc=Local"
+	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
